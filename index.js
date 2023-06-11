6693db1d85b09fac4feee4d0bad5b381465b09c4
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-12 00:15:42 +0300

    changed backend from flask to express

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 5000
+const ejs = require("ejs")
+
+app.set("view engine", "ejs")
+
+app.get("/", (req,res)=>{
+    res.render("home.ejs")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App runing on port ${PORT}`)
+})
