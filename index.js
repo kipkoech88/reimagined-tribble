a93c2d21d31c0ae71cdcb1656a3a3719b6f34777
kipkoech88 <hoseakipkoech88@gmail.com>
2023-06-12 01:37:40 +0300

    added signup, login, apply and fixed text alignment error

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get("/", (req,res)=>{
     res.render("home.ejs")
 })
 
+app.get("/login", (req,res)=>{
+    res.render("login.ejs")
+})
+
+app.get("/signup", (req,res)=>{
+    res.render("signup.ejs")
+})
+app.get("/apply", (req,res)=>{
+    res.render("apply.ejs")
+})
+
 app.listen(PORT, ()=>{
     console.log(`App runing on port ${PORT}`)
 })
